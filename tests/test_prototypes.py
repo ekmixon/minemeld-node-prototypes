34275be3b127bf90cde5463517e6d30d20ed6343
Sourcery AI <>
2022-08-02 18:48:46 +0000

    'Refactored by Sourcery'

--- tests/test_prototypes.py
+++ tests/test_prototypes.py
@@ -13,23 +13,32 @@ def _check_library(l):
     with open(l, 'r') as f:
         library = yaml.safe_load(f)
 
-    assert 'description' in library, "No description field in %s" % l
-    assert 'url' in library, "No url field in %s" % l
-    assert 'prototypes' in library, "No prototypes field in %s" % l
+    assert 'description' in library, f"No description field in {l}"
+    assert 'url' in library, f"No url field in {l}"
+    assert 'prototypes' in library, f"No prototypes field in {l}"
 
     for p, prototype in library['prototypes'].iteritems():
         LOG.debug('checking prototype %s', p)
-        assert 'development_status' in prototype, "No developement_status field in %s::%s" % (l, p)
-        assert 'author' in prototype, "No author field in %s::%s" % (l, p)
-        assert 'description' in prototype, "No description field in %s::%s" % (l, p)
-        assert 'class' in prototype, "No class field in %s::%s" % (l, p)
-        assert 'config' in prototype, "No config field in %s::%s" % (l, p)
-        assert 'node_type' in prototype, "No node_type field in %s::%s" % (l, p)
-        assert 'tags' in prototype, "No tags field in %s::%s" % (l, p)
-        assert isinstance(prototype['tags'], list), "Wrong type for attribute tags in %s::%s" % (l, p)
-        assert 'indicator_types' in prototype, "No indicator_types field in %s::%s" % (l, p)
-        assert isinstance(prototype['indicator_types'], list), "Wrong type for attribute indicator_types in %s::%s" % (l, p)
-        assert len(prototype['indicator_types']), "0 indicator_types in %s::%s" % (l, p)
+        assert (
+            'development_status' in prototype
+        ), f"No developement_status field in {l}::{p}"
+
+        assert 'author' in prototype, f"No author field in {l}::{p}"
+        assert 'description' in prototype, f"No description field in {l}::{p}"
+        assert 'class' in prototype, f"No class field in {l}::{p}"
+        assert 'config' in prototype, f"No config field in {l}::{p}"
+        assert 'node_type' in prototype, f"No node_type field in {l}::{p}"
+        assert 'tags' in prototype, f"No tags field in {l}::{p}"
+        assert isinstance(
+            prototype['tags'], list
+        ), f"Wrong type for attribute tags in {l}::{p}"
+
+        assert 'indicator_types' in prototype, f"No indicator_types field in {l}::{p}"
+        assert isinstance(
+            prototype['indicator_types'], list
+        ), f"Wrong type for attribute indicator_types in {l}::{p}"
+
+        assert len(prototype['indicator_types']), f"0 indicator_types in {l}::{p}"
 
 def test_prototypes():
     libraries = [os.path.join('prototypes', x) for x in os.listdir('prototypes')]
